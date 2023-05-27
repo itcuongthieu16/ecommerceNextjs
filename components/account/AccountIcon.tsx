92f775b31566c76739b47d20f99a814692b0f7b2
Cuong Thieu <it.thieutrancuong@gmail.com>
2023-05-27 16:00:07 +0700

    update

---
+++ components/account/AccountIcon.tsx
@@ -0,0 +1,13 @@
+import { AccountCircle } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import React from "react";
+
+const AccountIcon = () => {
+  return (
+    <IconButton size="large" edge="end" color="inherit">
+      <AccountCircle />
+    </IconButton>
+  );
+};
+
+export default AccountIcon;
